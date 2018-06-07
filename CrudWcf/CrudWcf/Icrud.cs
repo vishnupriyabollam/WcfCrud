4759c72c5169fc2cd1b4aa3f02872bd256f73a66
vishnupriyabollam <vishnupriya.bollam@neudesic.com>
2018-06-07 23:14:37 +0530

    crud wcf

---
+++ CrudWcf/CrudWcf/Icrud.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrudWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Icrud" in both code and config file together.
+    [ServiceContract]
+    public interface Icrud
+    {
+        [OperationContract]
+        List<Crud> Getcrud();
+
+        [OperationContract]
+        List<Crud> Addcrud(Crud addNewcrud);
+
+        [OperationContract]
+        List<Crud> Updatecrud(Crud updatecrud);
+
+        [OperationContract]
+        List<Crud> Deletecrud(int deletedcrudId);
+
+    }
+    [DataContract]
+    public class Crud
+    {
+        [DataMember]
+        public int crudId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public DateTime DOB { get; set; }  
+    }
+
+}
---
+++ CrudWcf/CrudWcf/crud.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrudWcf
+{
+    public class crud : Icrud
+    {
+        private static List<Crud> cruds = new List<Crud>()
+        {
+            new Crud {crudId=1,Name="vishnu",Age=20,DOB=new DateTime(2018,4,5)},
+            new Crud {crudId=2,Name="priya",Age=20,DOB=new DateTime(2018,6,30)}
+        };
+        public List<Crud> Addcrud(Crud addNewcrud)
+        {
+            cruds.Add(addNewcrud);
+            return cruds;
+        }
+        public List<Crud> Deletecrud(int deletedcrudId)
+        {
+            var deletingcrud = cruds.Find(t => t.crudId == deletedcrudId);
+            cruds.Remove(deletingcrud);
+            return cruds;
+        }
+
+        public List<Crud> Getcrud()
+        {
+            return cruds;
+        }
+
+        
+        
+
+        public List<Crud> Updatecrud(Crud updatedcrud)
+        {
+            var deletingcrud = cruds.Find(t => t.crudId == updatedcrud.crudId);
+            if (deletingcrud == null)
+                cruds.Add(updatedcrud);
+            else
+            {
+                cruds.Remove(deletingcrud);
+                cruds.Add(updatedcrud);
+            }
+            return cruds;
+        }
+    }
+}
